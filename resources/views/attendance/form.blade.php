6099d70c359c82711918107b337c63695c8c5e18
RissN <farisazizishie@gmail.com>
2026-03-12 09:43:04 +0700

    fix bug

--- resources/views/attendance/form.blade.php
+++ resources/views/attendance/form.blade.php
@@ -15,6 +15,64 @@
 </div>
 <div class="table-responsive shadow-sm mb-4">
     <table class="table table-hover align-middle mb-0">
-
+        <thead>
+            <tr>
+                <th width="5%" class="text-center">
+                    <div class="form-check d-flex justify-content-center mb-0">
+                        <input class="form-check-input" type="checkbox" name="check_all" id="check_all" title="Check All">
+                    </div>
+                </th>
+                <th width="20%" class="text-center">Student Name</th>
+                <th width="15%" class="text-center">Status In</th>
+                <th width="12%" class="text-center">Check In</th>
+                <th width="15%" class="text-center">Status Out</th>
+                <th width="12%" class="text-center">Check Out</th>
+                <th width="20%" class="text-center">Note</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ( $students as $index => $student )
+                <tr class="student-row">
+                    <td colspan="7" class="text-center">
+                        <div class="form-check d-flex justify-content-center mb-0">
+                            <input class="form-check-input" type="checkbox" name="attendances[{{ $index }}][student_id]" value="{{ $student->id }}" id="student_{{ $student->id }}" title="Check All">
+                        </div>
+                    </td>
+                    <td>
+                        <label for="student_{{ $student->id }}" class="mb-0 fw-semibold cursor-pointer d-block">{{ $student->name }}</label>
+                    </td>
+                    <td>
+                        <select name="attendances[{{ $index }}][status_in]" id="" disabled>
+                            <option value="">Select Status</option>
+                            <option value="hadir">Hadir</option>
+                            <option value="sakit">Sakit</option>
+                            <option value="izin">Izin</option>
+                            <option value="alpha">Alpha</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="time" name="attendances[{{ $index }}][check_in]" class="form-control check-in-time" disabled>
+                    </td>
+                    <td>
+                        <select name="attendances[{{ $index }}][status_in]" id="" disabled>
+                            <option value="">Select Status</option>
+                            <option value="pulang">Pulang</option>
+                            <option value="bolos">Bolos</option>
+                            <option value="izin pulang cepat">Izin Pulang Cepat</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="time" name="attendances[{{ $index }}][check_out]" class="form-control check-in-time" disabled>
+                    </td>
+                    <td>
+                        <input type="text" name="attendances[{{ $index }}][note]"  class="form-control note-input" placeholder="Optional..." disabled>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="7" align="center">Data Not Found. <a href="{{ route('student.create') }}">Create a Student</a> First</td>
+                </tr>
+            @endforelse
+        </tbody>
     </table>
 </div>
